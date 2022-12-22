a8e3038ae37d44b2996c508e9bcd3b9075066e13
Rafael <raf.frasson@gmail.com>
2022-12-21 23:32:47 -0300

    Updates

---
+++ precache-manifest.fb08b7d5042e462b319ae172fc1c0072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6da64cf99eb01f9c6eec30dbf247d7",
+    "url": "/starwars-planet-search/index.html"
+  },
+  {
+    "revision": "cb0f98883136eb7f6feb",
+    "url": "/starwars-planet-search/static/css/main.0c8dd27f.chunk.css"
+  },
+  {
+    "revision": "d63dfc119f2bb89dc890",
+    "url": "/starwars-planet-search/static/js/2.20f1c4a4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-planet-search/static/js/2.20f1c4a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb0f98883136eb7f6feb",
+    "url": "/starwars-planet-search/static/js/main.345bc591.chunk.js"
+  },
+  {
+    "revision": "7ae9ac9354f1d33a3553",
+    "url": "/starwars-planet-search/static/js/runtime-main.9d6297ae.js"
+  }
+]);
